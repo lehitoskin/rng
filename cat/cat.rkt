ac31060cd2c8f5322e9be01ae27847959c1e5cf6
Lehi Toskin <lehi@tosk.in>
2014-11-02 15:42:15 -0800

    cat takes any number of files

--- cat/cat.rkt
+++ cat/cat.rkt
@@ -6,6 +6,8 @@
 ; RNG cat
 
 (command-line
- #:args (file)
- (let [(lst (file->lines file))]
-   (for-each displayln lst)))
+ #:args (file . others)
+ (let [(lst-file (file->lines file))
+       (lst-others (map file->lines others))]
+   (for-each displayln lst-file)
+   (for-each (λ (x) (for-each displayln x)) lst-others)))
